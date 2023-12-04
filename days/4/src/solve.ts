b4f60b8f97748006c59ab39a5614b7584c703390
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-12-04 10:15:11 -0500

    feat: day 4 part b

--- days/4/src/solve.ts
+++ days/4/src/solve.ts
@@ -24,5 +24,35 @@ export const partA: Solver = (lines: string[]) => {
 };
 
 export const partB: Solver = (lines: string[]) => {
-  return 0;
+  let totalCards = 0;
+
+  const cardNumCopies: Record<number, number> = {};
+
+  for (let cardNum = 0; cardNum < lines.length; cardNum++) {
+    cardNumCopies[cardNum] = 1;
+  }
+
+  for (let cardNum = 0; cardNum < lines.length; cardNum++) {
+    const line = lines[cardNum];
+
+    let [winningNums, myNums] = line
+      .split(/:\s+|\s+\|\s+/)
+      .slice(1)
+      .map((part) => part.split(/\s+/).map((num) => parseInt(num)));
+
+    let winningSet = new Set(winningNums);
+
+    const wins = myNums.reduce(
+      (sum, cur) => (winningSet.has(cur) ? sum + 1 : sum),
+      0
+    );
+
+    for (let i = 0; i < wins; i++) {
+      cardNumCopies[cardNum + i + 1] += cardNumCopies[cardNum];
+    }
+
+    totalCards += cardNumCopies[cardNum];
+  }
+
+  return totalCards;
 };
