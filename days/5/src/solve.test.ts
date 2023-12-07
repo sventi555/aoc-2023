3af78710a8227f196d1848566e71b7576d6634d5
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-12-07 12:33:24 -0500

    feat: day 5 part b

--- days/5/src/solve.test.ts
+++ days/5/src/solve.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "bun:test";
-import { partA, partB } from "./solve";
+import { Range, partA, partB } from "./solve";
 
 describe("part A", () => {
   it("should pass", () => {
@@ -43,8 +43,183 @@ describe("part A", () => {
   });
 });
 
+describe("Range", () => {
+  describe("overlap", () => {
+    it("should return null for no overlap", () => {
+      const r1 = new Range(1, 5);
+      const r2 = new Range(6, 10);
+
+      expect(r1.overlap(r2)).toBeNull();
+      expect(r2.overlap(r1)).toBeNull();
+    });
+
+    it("should find overlap at either end", () => {
+      const r1 = new Range(1, 5);
+      const r2 = new Range(3, 8);
+
+      expect(r1.overlap(r2)).toEqual(new Range(3, 5));
+      expect(r2.overlap(r1)).toEqual(new Range(3, 5));
+    });
+
+    it("should find overlap with inclusive ends", () => {
+      const r1 = new Range(1, 5);
+      const r2 = new Range(1, 3);
+      const r3 = new Range(3, 5);
+
+      expect(r1.overlap(r2)).toEqual(new Range(1, 3));
+      expect(r2.overlap(r1)).toEqual(new Range(1, 3));
+
+      expect(r1.overlap(r3)).toEqual(new Range(3, 5));
+      expect(r3.overlap(r1)).toEqual(new Range(3, 5));
+
+      expect(r1.overlap(r1)).toEqual(new Range(1, 5));
+    });
+
+    it("should find fully contained overlap", () => {
+      const r1 = new Range(1, 5);
+      const r2 = new Range(2, 4);
+
+      expect(r1.overlap(r2)).toEqual(new Range(2, 4));
+      expect(r2.overlap(r1)).toEqual(new Range(2, 4));
+    });
+  });
+
+  describe("split shift", () => {
+    it("should work with no overlap", () => {
+      const r = new Range(1, 5);
+
+      const from = new Range(6, 10);
+      const to = new Range(7, 11);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [new Range(1, 5)],
+        changed: [],
+      });
+    });
+
+    it("should work with partial overlap at start", () => {
+      const r = new Range(3, 6);
+
+      const from = new Range(1, 4);
+      const to = new Range(11, 14);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [new Range(5, 6)],
+        changed: [new Range(13, 14)],
+      });
+    });
+
+    it("should work with partial overlap at end", () => {
+      const r = new Range(3, 6);
+      const from = new Range(5, 8);
+      const to = new Range(11, 14);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [new Range(3, 4)],
+        changed: [new Range(11, 12)],
+      });
+    });
+
+    it("should work with boundary overlap at start", () => {
+      const r = new Range(3, 6);
+
+      const from = new Range(3, 5);
+      const to = new Range(11, 13);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [new Range(6, 6)],
+        changed: [new Range(11, 13)],
+      });
+    });
+
+    it("should work with boundary overlap at end", () => {
+      const r = new Range(3, 6);
+
+      const from = new Range(4, 6);
+      const to = new Range(11, 13);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [new Range(3, 3)],
+        changed: [new Range(11, 13)],
+      });
+    });
+
+    it("should work with exact overlap", () => {
+      const r = new Range(3, 6);
+
+      const from = new Range(3, 6);
+      const to = new Range(11, 14);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [],
+        changed: [new Range(11, 14)],
+      });
+    });
+
+    it("should work with inner containment", () => {
+      const r = new Range(3, 6);
+
+      const from = new Range(1, 8);
+      const to = new Range(11, 18);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [],
+        changed: [new Range(13, 16)],
+      });
+    });
+
+    it("should work with outer containment", () => {
+      const r = new Range(1, 8);
+
+      const from = new Range(3, 6);
+      const to = new Range(11, 14);
+
+      expect(r.splitShift(from, to)).toEqual({
+        same: [new Range(1, 2), new Range(7, 8)],
+        changed: [new Range(11, 14)],
+      });
+    });
+  });
+});
+
 describe("part B", () => {
   it("should pass", () => {
-    expect(partB([])).toEqual(0);
+    expect(
+      partB([
+        "seeds: 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        "37 52 2",
+        "39 0 15",
+        "",
+        "fertilizer-to-water map:",
+        "49 53 8",
+        "0 11 42",
+        "42 0 7",
+        "57 7 4",
+        "",
+        "water-to-light map:",
+        "88 18 7",
+        "18 25 70",
+        "",
+        "light-to-temperature map:",
+        "45 77 23",
+        "81 45 19",
+        "68 64 13",
+        "",
+        "temperature-to-humidity map:",
+        "0 69 1",
+        "1 0 69",
+        "",
+        "humidity-to-location map:",
+        "60 56 37",
+        "56 93 4",
+      ])
+    ).toEqual(46);
   });
 });
--- days/5/src/solve.ts
+++ days/5/src/solve.ts
@@ -88,6 +88,110 @@ export const partA: Solver = (lines: string[]) => {
   return minLocation;
 };
 
+export class Range {
+  start: number;
+  end: number;
+
+  constructor(start: number, end: number) {
+    this.start = start;
+    this.end = end;
+  }
+
+  copy() {
+    return new Range(this.start, this.end);
+  }
+
+  overlap(other: Range) {
+    if (this.end < other.start || other.end < this.start) {
+      return null;
+    }
+
+    return new Range(
+      Math.max(this.start, other.start),
+      Math.min(this.end, other.end)
+    );
+  }
+
+  splitShift(from: Range, to: Range) {
+    const overlap = this.overlap(from);
+    if (overlap == null) {
+      return { same: [this.copy()], changed: [] };
+    }
+
+    if (overlap.start < this.start || overlap.end > this.end) {
+      throw new Error("cannot split outside of range");
+    }
+
+    const res: { same: Range[]; changed: Range[] } = { same: [], changed: [] };
+    if (overlap.start > this.start) {
+      res.same.push(new Range(this.start, overlap.start - 1));
+    }
+    const offset = to.start - from.start;
+    res.changed.push(new Range(overlap.start + offset, overlap.end + offset));
+    if (overlap.end < this.end) {
+      res.same.push(new Range(overlap.end + 1, this.end));
+    }
+
+    return res;
+  }
+}
+
+type Mapping = { src: Range; dest: Range };
+
 export const partB: Solver = (lines: string[]) => {
-  return 0;
+  const seedRanges: Range[] = [];
+  const mappings: Mapping[][] = [];
+
+  lines = [...lines, ""];
+  let group: string[] = [];
+  for (let lineNum = 0; lineNum < lines.length; lineNum++) {
+    const line = lines[lineNum];
+    if (/seeds:/.exec(line)) {
+      const seedNums = line
+        .split(": ")[1]
+        .split(" ")
+        .map((n) => parseInt(n));
+
+      for (let i = 0; i < seedNums.length; i += 2) {
+        const start = seedNums[i];
+        const d = seedNums[i + 1];
+        seedRanges.push(new Range(start, start + d - 1));
+      }
+      lineNum++;
+      continue;
+    }
+
+    if (line === "") {
+      mappings.push([]);
+      for (const rangeMapping of group.slice(1)) {
+        const [dest, src, range] = rangeMapping
+          .split(" ")
+          .map((n) => parseInt(n));
+        mappings.at(-1)!.push({
+          src: new Range(src, src + range - 1),
+          dest: new Range(dest, dest + range - 1),
+        });
+      }
+
+      group = [];
+    } else {
+      group.push(line);
+    }
+  }
+
+  let ranges = seedRanges;
+  for (const mappingGroup of mappings) {
+    const shifted: Range[] = [];
+    for (const mapping of mappingGroup) {
+      for (let rangeIndex = ranges.length - 1; rangeIndex >= 0; rangeIndex--) {
+        const range = ranges.splice(rangeIndex, 1)[0];
+        const { same, changed } = range.splitShift(mapping.src, mapping.dest);
+        ranges.push(...same);
+        shifted.push(...changed);
+      }
+    }
+    ranges.push(...shifted);
+  }
+
+  return ranges.reduce((min, range) => Math.min(range.start, min), Infinity);
 };
